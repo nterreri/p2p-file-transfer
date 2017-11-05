1b7dfd3f09f5f4c1ce9d6ab45d9ac5adb24816d7
nterreri <niccolo.terreri@gmail.com>
2017-11-05 16:47:30 +0000

    Refactor client

--- public/signaling/Answerer.js
+++ public/signaling/Answerer.js
@@ -1,67 +1,80 @@
 // jshint browser: true
 'use strict';
 
+let webSocket;
+let connection;
 let fileReceived;
 let fileReceivedHandler;
 
-export function registerAsAnswerer() {
-    const answererConnection = new RTCPeerConnection();
-    const answererWebSocket = new WebSocket('ws://localhost:8085', 'json');
+function setUpDataTransferSession(offererDescription) {
+    console.log('Setting up RTC session answer after receiving a message from the web socket.');
 
-    answererConnection.onicecandidate = (e) => {
+    connection.ondatachannel = (event) => {
+        console.info('Answerer received data channel');
+        listenForMessages(event.channel);
+    };
+
+    connection.setRemoteDescription(offererDescription);
+    connection.createAnswer().then(
+        descr => {
+            connection.setLocalDescription(descr);
+            webSocket.send(JSON.stringify(descr));
+            console.info('Sent answerer session description after setting offerer description as remote and local description.');
+        }
+    );
+}
+
+function listenForMessages(dataChannel) {
+    dataChannel.onmessage = (rtcMessage) => {
+        const data = rtcMessage.data;
+        console.info('Answerer received RTC message:', data);
+
+        if (data instanceof ArrayBuffer) {
+            fileReceived = new Blob([data]);
+            console.info('Answerer received file of size:', fileReceived.size);
+
+            fileReceivedHandler && fileReceivedHandler(fileReceived);
+        }
+    };
+}
+
+function parseSocketMessage(message) {
+    const payload = JSON.parse(message);
+    if (payload.type === 'offer') {
+        setUpDataTransferSession(payload);
+    }
+
+    if (payload.type === 'ICE') {
+        connection.addIceCandidate(payload.candidate).then(
+            () => console.info('Answerer added ICE candiate', payload.candidate.usernameFragment, 'successfully.'),
+            (error) => console.error('Answerer failed to add ICE candidate due to', error)
+        );
+    }
+}
+
+function setUpWebSocket() {
+    webSocket.onopen = () => {
+        console.log('Answerer connection with socket server opened, sending request to register as offer answerer.');
+
+        webSocket.send(JSON.stringify({type: 'registerAnswerer'}));
+    };
+
+    webSocket.onmessage = (e) => parseSocketMessage(e.data);
+}
+
+export function registerAsAnswerer() {
+    connection = new RTCPeerConnection();
+    webSocket = new WebSocket('ws://localhost:8085', 'json');
+
+    connection.onicecandidate = (e) => {
         if (!e.candidate) {
             return;
         }
 
-        answererWebSocket.send(JSON.stringify({type: 'ICE-answerer', candidate: e.candidate}));
+        webSocket.send(JSON.stringify({type: 'ICE-answerer', candidate: e.candidate}));
     };
 
-    answererWebSocket.onopen = () => {
-        console.log('Answerer connection with socket server opened, sending request to register as offer answerer.');
-
-        answererWebSocket.send(JSON.stringify({type: 'registerAnswerer'}));
-    };
-
-    answererWebSocket.onmessage = (e) => {
-        const payload = JSON.parse(e.data);
-        if (payload.type === 'offer') {
-            console.log('Setting up RTC session answer after receiving a message from the web socket.');
-
-            const offererDescription = payload;
-
-            answererConnection.ondatachannel = (event) => {
-                console.info('Answerer received data channel');
-
-                event.channel.onmessage = (rtcMessage) => {
-                    const data = rtcMessage.data;
-                    console.info('Answerer received RTC message:', data);
-
-                    if (data instanceof ArrayBuffer) {
-                        fileReceived = new Blob([data]);
-                        console.info('Answerer received file of size:', fileReceived.size);
-
-                        fileReceivedHandler && fileReceivedHandler(fileReceived);
-                    }
-                };
-            };
-
-            answererConnection.setRemoteDescription(offererDescription);
-            answererConnection.createAnswer().then(
-                descr => {
-                    answererConnection.setLocalDescription(descr);
-                    answererWebSocket.send(JSON.stringify(descr));
-                    console.info('Sent answerer session description after setting offerer description as remote and local description.');
-                }
-            );
-        }
-
-        if (payload.type === 'ICE') {
-            answererConnection.addIceCandidate(payload.candidate).then(
-                () => console.info('Answerer added ICE candiate', payload.candidate.usernameFragment, 'successfully.'),
-                (error) => console.error('Answerer failed to add ICE candidate due to', error)
-            );
-        }
-    };
+    setUpWebSocket();
 }
 
 export function onFileReceived(handler) {
--- public/signaling/Offerer.js
+++ public/signaling/Offerer.js
@@ -1,11 +1,50 @@
 // jshint browser: true
 'use strict';
+
+let webSocket;
 let connection;
 let dataChannel;
 
+function parseSocketMessage(message) {
+    const payload = JSON.parse(message);
+    if (payload.type === 'answer') {
+        console.log('Setting RTC session answer description after receiving an answer message from the web socket.');
+
+        const answererDescription = payload;
+        connection.setRemoteDescription(answererDescription);
+    }
+
+    if (payload.type === 'ICE') {
+        connection.addIceCandidate(payload.candidate).then(
+            () => console.info('Offerer added ICE candidate', payload.candidate.usernameFragment, 'successfully.'),
+            (error) => console.error('Offerer failed to add ICE candidate due to', error)
+        );
+    }
+}
+
+function initializeDataTransferSession() {
+    console.log('Setting up RTC session offer as connection to web socket has opened.');
+
+    dataChannel = connection.createDataChannel('demoChannel');
+    dataChannel.binarytype = 'arraybuffer';
+
+    connection.createOffer().then(
+        descr => {
+            connection.setLocalDescription(descr);
+            webSocket.send(JSON.stringify(descr));
+            console.log('Sent offerer session description after setting as local description.');
+        }
+    );
+}
+
+function setUpWebSocket() {
+    webSocket.onmessage = (e) => parseSocketMessage(e.data);
+    webSocket.onopen = initializeDataTransferSession;
+}
+
 export function sendOffer() {
-    const webSocket = new WebSocket('ws://localhost:8085', 'json');
     connection = new RTCPeerConnection();
+    webSocket = new WebSocket('ws://localhost:8085', 'json');
 
     connection.onicecandidate = (e) => {
         if (!e.candidate) {
@@ -15,37 +54,7 @@ export function sendOffer() {
         webSocket.send(JSON.stringify({type: 'ICE-offerer', candidate: e.candidate}));
     };
 
-    webSocket.onmessage = (e) => {
-        const payload = JSON.parse(e.data);
-        if (payload.type === 'answer') {
-            console.log('Setting RTC session answer description after receiving an answer message from the web socket.');
-
-            const answererDescription = payload;
-            connection.setRemoteDescription(answererDescription);
-        }
-
-        if (payload.type === 'ICE') {
-            connection.addIceCandidate(payload.candidate).then(
-                () => console.info('Offerer added ICE candidate', payload.candidate.usernameFragment, 'successfully.'),
-                (error) => console.error('Offerer failed to add ICE candidate due to', error)
-            );
-        }
-    };
-
-    webSocket.onopen = () => {
-        console.log('Setting up RTC session offer as connection to web socket has opened.');
-
-        dataChannel = connection.createDataChannel('demoChannel');
-        dataChannel.binarytype = 'arraybuffer';
-
-        connection.createOffer().then(
-            descr => {
-                connection.setLocalDescription(descr);
-                webSocket.send(JSON.stringify(descr));
-                console.log('Sent offerer session description after setting as local description.');
-            }
-        );
-    };
+    setUpWebSocket();
 }
 
 export function sendFile(file) {
